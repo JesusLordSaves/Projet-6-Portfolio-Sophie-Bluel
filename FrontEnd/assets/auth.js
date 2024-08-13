7c3c25e45e305dd87ca73c85a3fe2bed5e4d25db
Maxime Kaberuka <maximekaberuka2@gmail.com>
2024-08-13 17:51:29 +0200

    mdp auth modifié

--- FrontEnd/assets/auth.js
+++ FrontEnd/assets/auth.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('password').value;
 
         try {
-            const response = await fetch('http://localhost:5678/api/auth/login', {
+            const response = await fetch('http://localhost:5678/api/users/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
